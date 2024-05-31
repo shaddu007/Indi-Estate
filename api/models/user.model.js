96dc099e12c7c117721064e58bf116dc3677a3f2
shaddu007 <shadan.alam.ece24@heritageit.edu.in>
2024-06-01 02:16:39 +0530

    update header and profile pic

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true,
-    }
+    },
+    avatar:{
+        type: String,
+        default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw2zW7Omo27CZM5fQ_h_4HIF&ust=1717265590129000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCHhfu-uIYDFQAAAAAdAAAAABAE',
+    },
 }, {timestamps:true});
 const User = mongoose.model('User', userSchema);
 export default User;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,4 +4,4 @@ export default function Profile() {
   return (
     <div>Profile</div>
   )
-}
+};
